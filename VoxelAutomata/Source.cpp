d5255c4d4b628ac0853a3fb5583ce454a9ad666d
KiwiRobbie <r.b.christie@icloud.com>
2021-04-18 23:00:54 +1200

    Fixed Missing Project Folder

---
+++ VoxelAutomata/Source.cpp
@@ -0,0 +1,263 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <shader_s/shader_s.h>
+#include <camera/camera.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 0.0f));
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 90.0f;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+const int size = 64;
+glm::vec3 lightPos = glm::vec3(32,32,32);
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetWindowPos(window, 100, 100);
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile our shader zprogram
+    // ------------------------------------
+    Shader ourShader("shaders/shader.vert", "shaders/shader.frag","shaders/shader.geom");
+    Compute update("shaders/update.comp");
+    Compute swap("shaders/buffer_swap.comp");
+
+    Compute generate("shaders/generate.comp");
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, (size*size*size)*4*sizeof(float), NULL, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // texture coord attribute
+    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    unsigned int BBO, BAO;
+    glGenVertexArrays(1, &BAO);
+    glGenBuffers(1, &BBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, BBO);
+    glBufferData(GL_ARRAY_BUFFER, (size * size * size) * 4 * sizeof(float), NULL, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // texture coord attribute
+    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, VBO);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, BBO);
+
+
+    camera.MovementSpeed = 20.0f;
+    camera.Position = glm::vec3(0, 64, 0);
+    camera.Pitch = -40;
+    camera.Yaw = 45;
+    camera.ProcessMouseMovement(0, 0);
+    generate.use();
+    generate.setFloat("time", glfwGetTime());
+    glDispatchCompute((GLuint)size, (GLuint)size, (GLuint)size);
+    glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
+    bool buffer_switch = false;
+
+    // render loop
+    // -----------
+
+    float last_update = 0;
+    while (!glfwWindowShouldClose(window))
+    {
+        
+           
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(window);
+
+        // update voxels
+        // -------------
+        if(currentFrame-last_update>0.5f)
+        { 
+            last_update = currentFrame;
+            update.use();
+            update.setFloat("time", glfwGetTime());
+            glDispatchCompute((GLuint)size, (GLuint)size, (GLuint)size);
+            glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
+            swap.use();
+            glDispatchCompute((GLuint)size, (GLuint)size, (GLuint)size);
+            glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+        }
+        // render
+        // ------
+        glClearColor(0.0f,0.0f,0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        // activate shader
+        ourShader.use();
+
+        // pass projection matrix to shader (note that in this case it could change every frame)
+        glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        ourShader.setMat4("projection", projection);
+
+        // camera/view transformation
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("view", view);
+
+        // set lighting settings
+        ourShader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+        ourShader.setVec3("lightPos", glm::vec3(0,64,32));
+        ourShader.setVec3("viewPos", camera.Position);
+
+        // render boxes
+        glDrawArrays(GL_POINTS,0,size*size*size);
+        
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.ProcessKeyboard(UP, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+        camera.ProcessKeyboard(DOWN, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
---
+++ VoxelAutomata/shaders/shader.geom
@@ -0,0 +1,71 @@
+#version 460 core
+
+layout (points) in;
+in float State[];
+
+layout (triangle_strip, max_vertices = 24) out;
+
+out float state;
+out float light;
+out vec3 Normal;
+out vec3 FragPos;
+
+uniform mat4 view;
+uniform mat4 projection;
+
+
+const vec3 square[]={
+    vec3(-0.5, 0.5,-0.5),
+    vec3(-0.5, 0.5, 0.5),
+    vec3( 0.5, 0.5,-0.5),
+    vec3( 0.5, 0.5, 0.5)
+};
+
+const mat3 faces[]={
+mat3(1, 0, 0,
+     0, 1, 0,
+     0, 0, 1),
+
+mat3(0, 1,0,
+     0, 0,1,
+     1, 0,0),
+
+mat3(0, 0,1,
+     1, 0,0,
+     0, 1,0),
+
+mat3(1, 0, 0,
+     0,-1, 0,
+     0, 0, 1),
+
+mat3(0, 1, 0,
+     0, 0,-1,
+     1, 0, 0),
+
+mat3(0, 0, 1,
+    -1, 0, 0,
+     0, 1, 0)
+
+};
+
+
+void main(){    
+    if(State[0]>0.0){
+        for(int face = 0;face<6;face++)
+        {
+            for(int vertex = 0;vertex<4;vertex++)
+            {
+                vec4 aPos = gl_in[0].gl_Position+vec4(faces[face]*square[vertex],0);
+                gl_Position=projection*view*aPos;
+                state=State[0];
+                Normal=faces[face]*vec3(0,1,0);
+                FragPos = vec3( aPos);
+
+                light=1.0f;
+                
+                EmitVertex();
+            }
+            EndPrimitive();
+        }
+    }
+}
